0d9795dbc95d35419152c77369e433e6714d3407
fakhirula <fakhirula27@gmail.com>
2025-01-16 12:48:36 +0700

    fix: authapi

--- routes/api.php
+++ routes/api.php
@@ -15,35 +15,28 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api'); //md
 
+Route::apiResource('/genres', GenreController::class)->only(['index', 'show']);
+Route::apiResource('/authors', AuthorController::class)->only(['index', 'show']);
+Route::apiResource('/books', BookController::class)->only(['index', 'show']);
 
 Route::middleware(['auth:api'])->group(function () {
-    Route::get('/user', fn(Request $request) => $request->user()->id);
+    Route::get('/user', fn(Request $request) => $request->user());
+
+    Route::apiResource('/orders', OrderController::class)->only(['index', 'store']);
+    Route::apiResource('/payment_methods', OrderController::class)->only(['index']);
 
     Route::middleware(['role:admin,staff'])->group(function () {
-        // Route::apiResource('/books', BookController::class);
-        Route::apiResource('/genres', GenreController::class);
-        Route::apiResource('/authors', AuthorController::class);
+        Route::apiResource('/books', BookController::class)->only(['store', 'update', 'destroy']);
+        Route::apiResource('/genres', GenreController::class)->only(['store', 'update', 'destroy']);
+        Route::apiResource('/authors', AuthorController::class)->only(['store', 'update', 'destroy']);
 
-        Route::apiResource('/orders', OrderController::class);
-        Route::apiResource('/order_details', OrderDetailController::class);
+        Route::apiResource('/orders', OrderController::class)->only(['update', 'destroy']);
 
         Route::apiResource('/payment_methods', PaymentMethodController::class);
         Route::apiResource('/payments', PaymentController::class);
     });
 
-    Route::middleware(['role:admin,customer'])->group(function () {
-        Route::apiResource('/orders', OrderController::class)->only(['index', 'store']);
-        Route::apiResource('/payment_methods', OrderController::class)->only(['index']);
-    });
-
-
 });
-
-Route::apiResource('/books', BookController::class); // sementaraaaaaaaaaaa
-
-Route::apiResource('/books', BookController::class)->only(['index', 'show']);
-Route::apiResource('/genres', GenreController::class)->only(['index', 'show']);
-Route::apiResource('/authors', AuthorController::class)->only(['index', 'show']);
 
 
 
